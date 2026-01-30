d500fcea8eaf36082cfd56c6c7a04c24de7a29e0
Anastasia <nnaaastyyyaaa06@gmail.com>
2026-01-30 12:17:32 +0200

    Added modal window

--- task2/script.js
+++ task2/script.js
@@ -1,5 +1,7 @@
 const images = document.querySelectorAll(".photo");
 const body = document.querySelector("body");
+const modal = document.querySelector(".modal");
+const closeBtn = document.querySelector(".close");
 
 const options = {
   year: "numeric",
@@ -15,3 +17,21 @@ body.insertAdjacentHTML(
   "beforebegin",
   `<h1>Number of images: ${images.length}, Date: ${now}</h1>`,
 );
+
+images.forEach((image) => {
+  image.addEventListener("click", () => {
+    const srcImage = image.getElementsByTagName("img")[0].src;
+    const modalImg = modal.querySelector(".image");
+    modalImg.src = srcImage;
+    modal.style.display = "flex";
+  });
+});
+
+closeBtn.addEventListener("click", () => {
+  modal.style.display = "none";
+});
+
+body.addEventListener("click", (e) => {
+  if (e.target === modal && modal.style.display === "flex")
+    modal.style.display = "none";
+});
