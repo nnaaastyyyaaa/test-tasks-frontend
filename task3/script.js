b96d26aa29969a3b992ca7e495c2e61e6b2ec75a
Anastasia <nnaaastyyyaaa06@gmail.com>
2026-01-30 17:38:16 +0200

    Solved bonus(6) for task3

--- task3/script.js
+++ task3/script.js
@@ -2,19 +2,24 @@ const sendBtn = document.querySelector(".send-button");
 const table = document.querySelector(".universities-table");
 const tableBody = document.querySelector(".table-body");
 const resetBtn = document.querySelector(".reset-button");
+const saved = document.querySelector(".saved");
+const savedNum = document.querySelector(".number-of-saved");
+let savedCouner = 0;
 
 sendBtn.addEventListener("click", async () => {
   const country = document.querySelector(".country").value;
   const formattedCountry = country.trim().toLowerCase();
 
   tableBody.innerHTML = "";
+  savedCouner = 0;
+  saved.style.display = "none";
 
   try {
     const response = await fetch(
       `http://universities.hipolabs.com/search?country=${formattedCountry}`,
     );
 
-    if (!response) {
+    if (!response.ok) {
       throw new Error("Error while getting data");
     }
 
@@ -25,6 +30,7 @@ sendBtn.addEventListener("click", async () => {
     }
 
     table.style.display = "table";
+    saved.style.display = "block";
     let rowCount = 0;
 
     data.forEach((university) => {
@@ -36,6 +42,10 @@ sendBtn.addEventListener("click", async () => {
         <td>${university.domains}</td>
         <td>${university.name}</td>
         <td> <a href="${university.web_pages}" target="_blank">${university.web_pages}</a></td>
+        <td>
+           <input type="checkbox" name="favourite" class="favourite-checkbox"/>
+           <label>Add to my list</label>
+        </td>
 `;
       tableBody.appendChild(row);
     });
@@ -48,4 +58,16 @@ resetBtn.addEventListener("click", () => {
   table.style.display = "none";
   tableBody.innerHTML = "";
   document.querySelector(".country").value = "";
+});
+
+tableBody.addEventListener("change", (e) => {
+  if (e.target.classList.contains("favourite-checkbox")) {
+    if (e.target.checked) {
+      savedCouner++;
+      savedNum.textContent = savedCouner;
+    } else {
+      savedCouner--;
+      savedNum.textContent = savedCouner;
+    }
+  }
 });
